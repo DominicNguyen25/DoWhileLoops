4a2d0b148182a326a30df6d84818d458efe32b36
Dominic Nguyen <dominic.nguyen@mrjaffesclass.com>
2022-10-06 13:47:33 -0700

    10/6

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -13,21 +13,20 @@ public class RenameThisClass
         int a = 5;
         int b = 20;
 
-        while (i < 15) {
+        do {
             System.out.println("i is " + i + " but is less than 10 ");
             i += 1;
-        }
-        while (k > 5) {
+        } while (i < 15);
+        do {
             System.out.println("k is " + k + " but is greater than 5");
             k -= 1;
-        }
-        while (a < 10 && b <= 20 ) {
+        } while (k > 5);
+        do {
             System.out.println("a is " + a + " but is less than 10");
             System.out.println("b is " + b + " but less than or equal to 20");
             a += 1;
             b += 1;
-
-        }
+        } while (a < 10 && b <= 20 );
     }
 
     public static void main(String[] args)
